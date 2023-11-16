a46ebe45cdd9a04ad46b0063175d54aca7c99ed1
Caio lima <caiohenrys50@gmail.com>
2023-11-16 13:07:30 -0300

    feat: add theme light and provider

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material";
+import { blue, cyan } from "@mui/material/colors";
+
+
+export const LightTheme = createTheme({
+    palette: {
+        primary:{
+            main: blue[700],
+            dark: blue[800],
+            light: blue[500],
+            contrastText: "#FFFFFF",
+        },
+        secondary:{
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: "#FFFFFF",
+        },
+        background:{
+            default: "#F7F6F3",
+            paper: "#FFFFFF",
+        }
+    }
+}) 
