1ed36c19fdc7f44ad73f9dd48bfeb7ec72b59cab
Caio lima <caiohenrys50@gmail.com>
2023-11-18 23:49:36 -0300

    feat: add route dashboard

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import BasePageLayout from "../../shared/layout/BasePageLayout";
+
+export default function Dashboard() {
+  return (
+    <div>
+        <BasePageLayout/>
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
-import { Button } from "@mui/material";
 import {Navigate, Route, Routes } from "react-router-dom";
-import AuthContextDrawer from "../shared/context/AuthDrawer";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 export default function AppRoutes() {
 
-    const { toggleDrawerOpen } = AuthContextDrawer()
 
   return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button onClick={toggleDrawerOpen} variant="contained" color="primary">Toggle Drawer</Button>}/>
+            <Route path="/pagina-inicial" element={<Dashboard/>}/>
             <Route path="*" element={<Navigate to={"/pagina-inicial"} />}/>
         </Routes>
   )
