5fdf5f57992b36d613ddc8637ca527071de56e91
Caio lima <caiohenrys50@gmail.com>
2023-11-29 02:57:48 -0300

    refactor: name listing-tools

---
+++ src/shared/components/listing-tools/ListingTools.tsx
@@ -0,0 +1,63 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+
+
+interface IListingToolsProps{
+    searchText?: string
+    showInputSearch?: boolean
+    changingSearchText?: ( newText: string ) => void
+
+    textNewButton?: string
+    showNewButton?:  boolean
+    isNewClick?: () => void
+
+}
+
+export default function ListingTools({
+    searchText = '',
+    showInputSearch = false, 
+    changingSearchText,
+    textNewButton = 'novo',
+    showNewButton = false,
+    isNewClick
+}
+    : IListingToolsProps) {
+
+    const theme = useTheme()
+
+  return (
+    <Box component={Paper}
+        gap={1}
+        padding={1}
+        height={theme.spacing(5)}
+        marginX={1}
+        paddingX={2}
+        alignItems="center"
+        display="flex"
+    >
+
+        {showInputSearch && (
+            <TextField 
+                size="small"
+                placeholder="Pesquisar..."
+                value={searchText}
+                onChange={(event) => changingSearchText?.(event.target.value)}    
+            />
+        )}
+        <Box 
+            flex={1} 
+            display={"flex"} 
+            justifyContent={"end"}
+        >
+            {(showNewButton &&               
+                <Button 
+                    variant="contained"
+                    endIcon={<Icon>add</Icon>}
+                    onClick={isNewClick}
+                >
+                    {textNewButton}
+                </Button>
+            )}
+        </Box>
+    </Box>
+  )
+}
