e42037a972c6501b30f4445c0664432a44d792c5
Caio lima <caiohenrys50@gmail.com>
2023-12-01 14:58:39 -0300

    feat: add route cities

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import {Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/dashboard/Dashboard";
 import AuthContextDrawer from "../shared/context/AuthDrawer";
 import { useEffect } from "react";
+import ListOfCities from "../pages/cities/ListOfCities";
 
 export default function AppRoutes() {
 
@@ -14,12 +15,18 @@ export default function AppRoutes() {
           path: '/pagina-inicial',
           label: 'Pagina inicial'
         },    
+        {
+          icon: 'location_city',
+          path: '/cidades',
+          label: 'Cidades'
+        },    
       ])
     }, [setDrawerOptions])
 
   return (
         <Routes>
             <Route path="/pagina-inicial" element={<Dashboard/>}/>
+            <Route path="/cidades" element={<ListOfCities/>}/>
             <Route path="*" element={<Navigate to={"/pagina-inicial"} />}/>
         </Routes>
   )
